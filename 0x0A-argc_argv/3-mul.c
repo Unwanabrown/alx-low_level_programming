a88780f746c6072a5dbd413ebc0bd484dfb4ec85
unwana <unwanabrown10@gmail.com>
2023-04-08 04:51:33 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-*main - adds positive numbers
+* main - prints multiplication
+*of two numbers
 *@argc: number of arguments
 *@argv: array of arguments
-*Return: 0 on success, 1 on failure
+*Return: returns 0
 */
 
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int i, j, mul;
 
-	for (i = 1; i < argc; i++)
+	if (argc <= 2)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-
-		sum += atoi(argv[i]);
+		printf("Error\n");
+		return (1);
 	}
 
-	printf("%d\n", sum);
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	mul = i * j;
 
+	printf("%d\n", mul);
 	return (0);
 }
